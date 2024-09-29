328bc0c10f178d38442da97ececd03e5b90738b1
Mehedi Hasan <mehediinform@gmail.com>
2024-09-30 00:53:19 +0600

    Second Commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.myapplication;
 
+import android.app.SearchManager;
 import android.os.Bundle;
+import android.service.autofill.OnClickAction;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,43 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button1,button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        button1 = findViewById(R.id.button1Id);
+        button2 = findViewById(R.id.button2Id);
+
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view.getId() == R.id.button1Id){
+
+            Toast.makeText(MainActivity.this,"Hi Button One",Toast.LENGTH_SHORT).show();
+
+
+        }if (view.getId() == R.id.button2Id){
+
+            Toast.makeText(MainActivity.this,"Hi Button Two",Toast.LENGTH_SHORT).show();
+
+
+        }
+
+
     }
 }
